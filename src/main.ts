bfa110a1a0e3b1ef866f385ce153b946241eb259
mikorzen <michal.korzen-tech@pm.me>
2025-07-28 01:26:38 +0200

    too much to convey in a commit msg; total overhaul

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,5 @@
 import copy from 'copy-to-clipboard';
-
-function changeTab(event: MouseEvent, targetClass: string) {
-    let currentTab = document.querySelector('.' + targetClass) as HTMLElement;
-    currentTab?.classList.remove(targetClass);
-
-    let newTab = event.currentTarget as HTMLElement;
-    newTab.classList.add(targetClass);
-}
-
-function changeNavbarTab(event: MouseEvent) {
-    changeTab(event, 'navbar-tab-active');
-}
-
-function changeActiveTab(event: MouseEvent) {
-    changeTab(event, 'tab-active');
-}
+import htmx from 'htmx.org';
 
 (window as any).copy = copy;
-(window as any).changeNavbarTab = changeNavbarTab;
-(window as any).changeActiveTab = changeActiveTab;
+(window as any).htmx = htmx;
