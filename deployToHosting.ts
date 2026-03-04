d8388487282e3ba5a8282d3c5e861cf96b457f13
mikorzen <michal.korzen-tech@pm.me>
2026-03-04 16:04:20 +0100

    runner attempt

--- deployToHosting.ts
+++ deployToHosting.ts
@@ -2,14 +2,13 @@ import { deploy } from "@samkirkland/ftp-deploy";
 
 async function deployToHosting() {
     await deploy({
-        protocol: "ftps",
         server: process.env.FTP_SERVER!,
         username: process.env.FTP_USERNAME!,
         password: process.env.FTP_PASSWORD!,
         "local-dir": "./dist/",
         "server-dir": "/domains/sylviadieta.pl/public_html/",
         timeout: 20000,
-        // "dangerous-clean-slate": true,
+        "dangerous-clean-slate": true,
         // "dry-run": true,
         // "log-level": "verbose",
     });
