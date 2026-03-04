01184c0b5cf163b6aecaf864eaba50a8af140f80
mikorzen <michal.korzen-tech@pm.me>
2026-03-04 13:27:10 +0100

    SEO optimizations

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import compressor from "astro-compressor";
+import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+    site: "https://sylviadieta.pl",
+    trailingSlash: "never",
     vite: {
         plugins: [tailwindcss()],
     },
     prefetch: {
         prefetchAll: true,
     },
-    integrations: [compressor({gzip: false, brotli: true, zstd: false})],
+    integrations: [
+        compressor({gzip: false, brotli: true, zstd: false}),
+        sitemap()
+    ],
 });
